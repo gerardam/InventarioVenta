aedd483405efb633162789a6d3e31b4631e2c93f
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2022-02-25 15:23:59 -0600

    Creacion de proyectos para la separacion de capas; carpeta data es enviada a la capa IV.AccedoDatos

--- IV.AccesoDatos/Data/ApplicationDbContext.cs
+++ IV.AccesoDatos/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace InventarioVenta.Data
+namespace IV.AccesoDatos.Data
 {
 	public class ApplicationDbContext : IdentityDbContext
 	{
--- InventarioVenta/Startup.cs
+++ InventarioVenta/Startup.cs
@@ -1,4 +1,4 @@
-using InventarioVenta.Data;
+using IV.AccesoDatos.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
