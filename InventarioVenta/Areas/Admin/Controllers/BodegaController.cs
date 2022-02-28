99a62348b172ecfbdec7eea5987416f377599efb
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2022-02-28 09:49:17 -0600

    Diseno de tabla de datos utilizando librerias de Datatable de js

---
+++ InventarioVenta/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,30 @@
+﻿using IV.AccesoDatos.Repositorio.IRepositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioVenta.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public IActionResult ObtenerTodos()
+        {
+            var todos = _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+        #endregion
+    }
+}
---
+++ InventarioVenta/wwwroot/js/Bodega.js
@@ -0,0 +1,43 @@
+﻿var datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    datatable = $('#tblDatos').DataTable({
+        "ajax": {
+            "url": "/Admin/Bodega/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "nombre", "width": "20%" },
+            { "data": "descripcion", "width": "40%" },
+            {
+                "data": "estado",
+                "render": function (data) {
+                    if (data == true) {
+                        return "Activo";
+                    }
+                    else {
+                        return "Inactivo";
+                    }
+                }, "width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="Admin/Bodega/Upsert/${data}" class="btn btn-primary text-white" style="cursor:pointer">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <a class="btn btn-danger text-white" style="cursor:pointer">
+                                <i class="fas fa-trash"></i>
+                            </a>
+                        </div>
+                        `;
+                }, "width": "20%"
+            }
+        ]
+    });
+}
