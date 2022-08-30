3a2d25a40e1359f8d162c74b019d78d39a61fa02
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2022-08-30 12:22:37 -0500

    Carro de compras, procesamiento de pagos.

--- IV.Utilidades/DS.cs
+++ IV.Utilidades/DS.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace IV.Utilidades
 {
-	public static class DS
-	{
-		public const string Role_Cliente = "Cliente";
-		public const string Role_Admin = "Admin";
-		public const string Role_Inventario = "Inventario";
-		public const string Role_Ventas = "Ventas";
-		public const string ssCarroCompras = "Sesion Carro Compras";
-	}
+    public static class DS
+    {
+        public const string Role_Cliente = "Cliente";
+        public const string Role_Admin = "Admin";
+        public const string Role_Inventario = "Inventario";
+        public const string Role_Ventas = "Ventas";
+        public const string ssCarroCompras = "Sesion Carro Compras";
+
+        public const string EstadoPendiente = "Pendiente";
+        public const string EstadoAprobado = "Aprobado";
+        public const string EstadoEnProceso = "Procesando";
+        public const string EstadoEnviado = "Enviado";
+        public const string EstadoCancelado = "Cancelado";
+        public const string EstadoDevuelto = "Devuelto";
+
+        public const string PagoEstadoPendiente = "Pendiente";
+        public const string PagoEstadoAprobado = "Aprobado";
+        public const string PagoEstadoRetrasado = "Retrasado";
+        public const string EstadoRechazado = "Rechazado";
+    }
 }
--- InventarioVenta/Areas/Inventario/Controllers/CarroController.cs
+++ InventarioVenta/Areas/Inventario/Controllers/CarroController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using System;
 using System.Linq;
 using System.Security.Claims;
 
@@ -18,6 +20,7 @@ namespace InventarioVenta.Areas.Inventario.Controllers
         private readonly IEmailSender _emailSender;
         private readonly UserManager<IdentityUser> _userManager;
 
+        [BindProperty]
         public CarroComprasViewModel CarroComprasVM { get; set; }
 
         public CarroController(IUnidadTrabajo unidadTrabajo, IEmailSender emailSender, UserManager<IdentityUser> userManager)
@@ -88,6 +91,113 @@ namespace InventarioVenta.Areas.Inventario.Controllers
             HttpContext.Session.SetInt32(DS.ssCarroCompras, numeroProductos - 1);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Proceder()
+        {
+            var claimIdentidad = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentidad.FindFirst(ClaimTypes.NameIdentifier);
+
+            CarroComprasVM = new CarroComprasViewModel()
+            {
+                Orden = new Orden(),
+                CarroComprasLista = _unidadTrabajo.CarroCompras.ObtenerTodos(u => u.UsuarioAplicacionId ==
+                                                                claim.Value, incluirPropiedades: "Producto")
+            };
+
+            CarroComprasVM.Orden.TotalOrden = 0;
+            CarroComprasVM.Orden.UsuarioAplicacion = _unidadTrabajo.UsuarioAplicacion.ObtenerPrimero(u => u.Id == claim.Value);
+
+            foreach (var lista in CarroComprasVM.CarroComprasLista)
+            {
+                lista.Precio = lista.Producto.Precio;
+                CarroComprasVM.Orden.TotalOrden += (lista.Precio * lista.Cantidad);
+            }
+
+            CarroComprasVM.Orden.NombresCliente = CarroComprasVM.Orden.UsuarioAplicacion.Nombre + " " + CarroComprasVM.Orden.UsuarioAplicacion.Apellidos;
+            CarroComprasVM.Orden.Telefono = CarroComprasVM.Orden.UsuarioAplicacion.PhoneNumber;
+            CarroComprasVM.Orden.Direccion = CarroComprasVM.Orden.UsuarioAplicacion.Direccion;
+            CarroComprasVM.Orden.Pais = CarroComprasVM.Orden.UsuarioAplicacion.Pais;
+            CarroComprasVM.Orden.Ciudad = CarroComprasVM.Orden.UsuarioAplicacion.Ciudad;
+
+            return View(CarroComprasVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Proceder")]
+        public IActionResult ProcederPost(string stripeToken)
+        {
+            var claimIdentidad = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentidad.FindFirst(ClaimTypes.NameIdentifier);
+            CarroComprasVM.Orden.UsuarioAplicacion = _unidadTrabajo.UsuarioAplicacion.ObtenerPrimero(c => c.Id == claim.Value);
+            CarroComprasVM.CarroComprasLista = _unidadTrabajo.CarroCompras.ObtenerTodos(
+                                                    c => c.UsuarioAplicacionId == claim.Value, incluirPropiedades: "Producto");
+            CarroComprasVM.Orden.EstadoOrden = DS.EstadoPendiente;
+            CarroComprasVM.Orden.EstadoPago = DS.PagoEstadoPendiente;
+            CarroComprasVM.Orden.UsuarioAplicacionId = claim.Value;
+            CarroComprasVM.Orden.FechaOrden = DateTime.Now;
+
+            _unidadTrabajo.Orden.Agregar(CarroComprasVM.Orden);
+            _unidadTrabajo.Guardar();
+
+            foreach (var item in CarroComprasVM.CarroComprasLista)
+            {
+                OrdenDetalle ordenDetalle = new OrdenDetalle()
+                {
+                    ProductoId = item.ProductoId,
+                    OrdenId = CarroComprasVM.Orden.Id,
+                    Precio = item.Producto.Precio,
+                    Cantidad = item.Cantidad
+                };
+                CarroComprasVM.Orden.TotalOrden += ordenDetalle.Cantidad * ordenDetalle.Precio;
+                _unidadTrabajo.OrdenDetalle.Agregar(ordenDetalle);
+
+            }
+            // Remover los productos del carro de Compras
+            _unidadTrabajo.CarroCompras.RemoverRango(CarroComprasVM.CarroComprasLista);
+            _unidadTrabajo.Guardar();
+            HttpContext.Session.SetInt32(DS.ssCarroCompras, 0);
+
+            if (stripeToken == null)
+            {
+
+            }
+            else
+            {
+                //Procesar el Pago
+                var options = new ChargeCreateOptions
+                {
+                    Amount = Convert.ToInt32(CarroComprasVM.Orden.TotalOrden * 100),
+                    Currency = "usd",
+                    Description = "Numero de Orden: " + CarroComprasVM.Orden.Id,
+                    Source = stripeToken
+                };
+                var service = new ChargeService();
+                Charge charge = service.Create(options);
+                if (charge.BalanceTransactionId == null)
+                {
+                    CarroComprasVM.Orden.EstadoPago = DS.EstadoRechazado;
+                }
+                else
+                {
+                    CarroComprasVM.Orden.TransaccionId = charge.Id;
+                }
+                if (charge.Status.ToLower() == "succeeded")
+                {
+                    CarroComprasVM.Orden.EstadoPago = DS.PagoEstadoAprobado;
+                    CarroComprasVM.Orden.EstadoOrden = DS.EstadoAprobado;
+                    CarroComprasVM.Orden.FechaPago = DateTime.Now;
+                }
+
+            }
+            _unidadTrabajo.Guardar();
+            return RedirectToAction("OrdenConfirmacion", "Carro", new { id = CarroComprasVM.Orden.Id });
+        }
+
+        public IActionResult OrdenConfirmacion(int id)
+        {
+            return View(id);
         }
     }
 }
